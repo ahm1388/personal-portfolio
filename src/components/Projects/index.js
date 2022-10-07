120fe0ac93513882cccb3f5614f5844e45e0f1cc
Ahmed Al-Sabounchi <a_m_96@live.com>
2022-10-07 09:06:35 -0400

    Temporarily removed travel advisor website

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -15,17 +15,16 @@ import { faNodeJs, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
 const Projects = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
   const [projInd, setProjInd] = useState(0)
-  const images = [img_2, img_0, img_1, img_4, img_3]
-  const proj_names = ["Kitaby Book Club", "Traveler Chronicles", "Travel Advisor", "Hand Gesture Recognition", "Iron Ore Prediction"]
+  const images = [img_2, img_0, img_4, img_3]
+  const proj_names = ["Kitaby Book Club", "Traveler Chronicles", "Hand Gesture Recognition", "Iron Ore Prediction"]
   const proj_desc = [
     "Book group website where a user can register an account and create discussion groups for their favourite books, or join existing groups in the books they are interested in. The website was created using the MERN stack.",
     "Social media app where users can create accounts or log in through their Google account to share their travel adventures and like other peoples' experiences. Used React for the front end, NodeJS for the back end, and MongoDB to store the user and post information.",
-    "Used the Travel Advisor and Google APIs to create a web app that displays nearest restaurants, hotels, and attractions along with their ratings, contact information, and a link to their website. Also includes a search feature to look at locations from all around the world.",
     "Created a CNN model that was trained on approximately 10,000 images to be able to categorize pictures of hand gestures into 10 differnt types. Utilized popular data analysis and machine learning libraries in Python such as Numy, Pandas, and TensorFlow",
     "Used Random Forest Regression to train a model that predicts impurity concentration in ores acquired through a mining process. Lab tests require 2 hours to report the impurity level, which created a need for accurate, real-time impurity reporting based on process conditions. Model is also able to predict impurity concentration 3 hours in advance based on current conditions."
   ]
-  const proj_links = [["http://kitabybookclub-com.preview-domain.com/", "https://github.com/ahm1388/kitaby"], ["https://www.travelerchronicles.net", "https://github.com/ahm1388/traveler-chronicles"], ["https://main.d1jru81u0oynz1.amplifyapp.com/","https://github.com/ahm1388/travel-advisor"], ["https://github.com/ahm1388/ML-Hand-Gesture-Recognition", "https://github.com/ahm1388/ML-Hand-Gesture-Recognition"], ["https://github.com/ahm1388/quality-prediction-mining-process", "https://github.com/ahm1388/quality-prediction-mining-process"]]
-  const proj_tech = [[["React", faReact, '#5ED4F4'], ["NodeJS", faNodeJs, '#3c873a'], ["MongoDB", faDatabase, '#fff']], [["React", faReact, '#5ED4F4'], ["NodeJS", faNodeJs, '#3c873a'], ["MongoDB", faDatabase, '#fff']], [["React", faReact, '#5ED4F4']], [["Python", faPython, 'white']], [["Python", faPython, 'white']]]
+  const proj_links = [["http://kitabybookclub-com.preview-domain.com/", "https://github.com/ahm1388/kitaby"], ["https://www.travelerchronicles.net", "https://github.com/ahm1388/traveler-chronicles"], ["https://github.com/ahm1388/ML-Hand-Gesture-Recognition", "https://github.com/ahm1388/ML-Hand-Gesture-Recognition"], ["https://github.com/ahm1388/quality-prediction-mining-process", "https://github.com/ahm1388/quality-prediction-mining-process"]]
+  const proj_tech = [[["React", faReact, '#5ED4F4'], ["NodeJS", faNodeJs, '#3c873a'], ["MongoDB", faDatabase, '#fff']], [["React", faReact, '#5ED4F4'], ["NodeJS", faNodeJs, '#3c873a'], ["MongoDB", faDatabase, '#fff']], [["Python", faPython, 'white']], [["Python", faPython, 'white']]]
   const [windowDimensions, setWindowDimensions] = useState({
     winWidth: window.innerWidth,
     winHeight: window.innerHeight,
